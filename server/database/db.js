057480b3a010a3e818d6e1f5c2697398568f927a
Mohit Gupta <135738292+mtg718@users.noreply.github.com>
2023-11-21 12:28:55 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export const connectDB=async()=>{
     
-const MONGO_URI =
-  "mongodb+srv://mtg123:7877676528@cluster0.pvsrpea.mongodb.net/?retryWrites=true&w=majority";
+const MONGO_URI= process.env.MONGO_URI
     try {
        await mongoose.connect(MONGO_URI,{useNewUrlParser:true})
        console.log('Database is connected')
