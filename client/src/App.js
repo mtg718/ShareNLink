9911895f869da04afe9193b5df4a334662e3faa1
Mohit Gupta <135738292+mtg718@users.noreply.github.com>
2023-11-22 11:39:21 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import {useRef, useState,useEffect} from 'react'
 import './App.css';
 import {uploadFile }from './uploads/api.js'
+
+
 function App() {
   const [file,setFile]= useState('');
   const [result,setResult]= useState('');
+  const [loading,setLoading] = useState(false);
   const fileInputRef = useRef();
 const background =
   "https://smehelper.vastraapp.com/assets/marketing-tools/images/SMEhelperPage/visiting-card.png";
@@ -16,9 +19,18 @@ const background =
         data.append("name",file.name);
         data.append("file",file);
 
-      let response= await uploadFile(data);
-  setResult(response.path);
+        setLoading(true);
+        setResult('');
+     try {
+          let response = await uploadFile(data);
+          setResult(response.path);
+        } catch (error) {
+          console.error('Error while uploading file:', error.message);
+        } finally {
+          setLoading(false);
+        }
       }
+      
     }
     getImage();
   },[file])
@@ -30,17 +42,27 @@ fileInputRef.current.click();
 console.log(file);
   return (
     <div className="App">
-      <img src={background} />
+      <img id="bg" src={background} />
       <div className="box">
-        <h1>Simple File Sharing!</h1>
-        <h3>Upload and share the download link</h3>
-        <button onClick={()=>handleUpload()}>Upload</button>
-        <input type="file" 
-        style={{display:"none"}}
-        ref={fileInputRef}
-        onChange={(e)=>setFile(e.target.files[0])}/>
+        <h1>Share Your Files!</h1>
+        <h3>Upload and share the download link!</h3>
+        <button onClick={() => handleUpload()}>Upload</button>
+        <input
+          type="file"
+          style={{ display: "none" }}
+          ref={fileInputRef}
+          onChange={(e) => setFile(e.target.files[0])}
+        />
 
-        <a href={result} target="_blank">{result}</a>
+        {loading && <h2>Loading...</h2>}
+        {result && (
+          <div id="link">
+            <h4 id="link-para">Get your generated link...</h4>
+            <a id="link-data" href={result} target="_blank">
+              {result}
+            </a>
+          </div>
+        )}
       </div>
     </div>
   );
